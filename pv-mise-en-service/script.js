331f7b560262237eebb69d40acd89737112e99c9
ilesduponant <energies.duponant@outlook.fr>
2025-05-05 22:53:30 +0200

    Add files via upload

--- pv-mise-en-service/script.js
+++ pv-mise-en-service/script.js
@@ -281,7 +281,7 @@ document.addEventListener("DOMContentLoaded", () => {
 async function genererPDF(data) {
     
 	const { jsPDF } = window.jspdf;
-    const pdf = new jsPDF();
+    const pdf = new jsPDF({compress: true});
 	
 	if (!hasSignature) {
 		event.preventDefault(); // Empêche toute action par défaut, si nécessaire
